93cc1d8cdc6614a3c0fce59b6c4d0d8830d63937
gravitustahir <99106044+gravitustahir@users.noreply.github.com>
2022-05-31 14:57:29 +0500

    metasoft

---
+++ Metasoft/index.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function () {
+    $(".hamburger").click(function(){
+          $(".toggleClass, .icon, .nav-btn").toggle(300);
+    });
+});
+$("document").ready(function() {
+    $(".icon").click(function(){
+        window.open("index.html");
+});
+});
+$("document").ready(function() {
+    $(".nav-btn").click(function(){
+        window.open("contact.html");
+});
+});
+$("document").ready(function() {
+    $(".mem").click(function(){
+        window.open("ratecard.html");
+});
+});
